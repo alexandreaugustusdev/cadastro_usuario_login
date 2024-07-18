2af35557ea2456406d9ca4e801ca863f0eec6f88
augustusborges <alexandre.borges@stoneage.com.br>
2024-07-18 15:46:43 -0300

    Validacao e Tratamento de erros do cadastro do usuario

--- src/controller/usuarioController.ts
+++ src/controller/usuarioController.ts
@@ -1,35 +1,57 @@
-import { Request, Response } from 'express'
-import { Usuario } from '../@types/usuario.js'
-import { UsuarioModel } from '../model/usuarioModel.js'
+import Joi from 'joi';
+import { Request, Response } from 'express';
+import { Usuario } from '../@types/usuario.js';
+import { UsuarioModel } from '../model/usuarioModel.js';
 
 export class UsuarioController {
-  private usuarioModel: UsuarioModel
+    async criar(req: Request, res: Response) {
+        try {
+            const usuarioModel = montaUsuarioModel();
+            const valida = await validaEntrada(req.body);
 
-  constructor() {
-    this.usuarioModel = new UsuarioModel()
-  }
+            const { nome, email, senha } = req.body as unknown as Usuario;
 
-  async criar(req: Request, res: Response) {
-    const { nome, email, senha } = req.body as unknown as Usuario
+            if ((await usuarioModel.buscarPorEmail(email)) != undefined && (await usuarioModel.buscarPorEmail(email)))
+                throw { code: 15, message: 'Usuário já cadastrado no sistema' };
 
-    const usuario: Usuario = await this.usuarioModel.criar({
-      nome,
-      email,
-      senha
-    })
+            const usuario: Usuario = await usuarioModel.criar({ nome, email, senha });
 
-    return res.status(201).json(usuario)
-  }
+            return res.status(201).json({ status: 'OK', ...usuario });
+        } catch (err) {
+            if (err.code == 15) {
+                return res.status(401).json({ status: 'NOK', message: err.message });
+            }
+            return res.status(400).json({ status: 'NOK', message: err.message });
+        }
+    }
 
-  async login(req: Request, res: Response) {
-    const { nome, email, senha } = req.body as unknown as Usuario
+    async login(req: Request, res: Response) {
+        const { nome, email, senha } = req.body as unknown as Usuario;
 
-    const usuario: Usuario = await this.usuarioModel.criar({
-      nome,
-      email,
-      senha
-    })
+        const usuario: Usuario = await montaUsuarioModel().login({ nome, email, senha });
 
-    return res.status(200).json(usuario)
-  }
+        return res.status(200).json(usuario);
+    }
+}
+
+async function validaEntrada(usuario: Usuario): Promise<string> {
+    try {
+        if (!usuario.nome.length || !usuario.email.length || !usuario.senha.length) throw new Error('Todos os campos são obrigatórios');
+
+        const schema = Joi.object({
+            nome: Joi.string().required().min(3).max(30),
+            email: Joi.string().email().required(),
+            senha: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+        });
+
+        const value = await schema.validateAsync(usuario);
+        return 'sim';
+    } catch (err) {
+        throw new Error(err.message);
+    }
+}
+
+function montaUsuarioModel() {
+    const usuarioModel: UsuarioModel = new UsuarioModel();
+    return usuarioModel;
 }
--- src/model/usuarioModel.ts
+++ src/model/usuarioModel.ts
@@ -1,19 +1,31 @@
-import { Usuario } from '../@types/usuario.js'
-import { dbConn } from '../config/dbConnect.js'
+import { Usuario } from '../@types/usuario.js';
+import { dbConn } from '../config/dbConnect.js';
 
 export class UsuarioModel {
-  async criar(usuario: Usuario) {
-    const retorno = await dbConn('usuario').insert({
-      nome: usuario.nome,
-      email: usuario.email,
-      senha: usuario.senha
-    })
+    async criar(usuario: Usuario) {
+        try {
+            const retorno = await dbConn('usuario').insert({ nome: usuario.nome, email: usuario.email, senha: usuario.senha });
 
-    return usuario
-  }
+            return usuario;
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
 
-  async login(usuario: Usuario) {
-    const retorno = await dbConn('usuario').where({ email: usuario.email, senha: usuario.senha }).select('email', 'senha').first()
-    return retorno
-  }
+    async login(usuario: Usuario) {
+        const retorno = await dbConn('usuario').where({ email: usuario.email, senha: usuario.senha }).select('email', 'senha').first();
+        return retorno;
+    }
+
+    async buscarPorEmail(email: string) {
+        try {
+            const retorno = await dbConn('usuario').select().where('email', email).first();
+            console.log(retorno);
+            return retorno;
+        } catch (err) {
+            if (err.code == 'ECONNREFUSED') {
+                throw { code: 1, message: 'Erro ao conectar com o banco de dados. Favor contatar o Administrador do sistema.' };
+            }
+        }
+    }
 }
